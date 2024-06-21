ad42fdc7ae3efc47e0be0a1000aab21738741d2f
Vitor de Melo Nunes <vitor.nunes@gruposantander.com>
2024-06-21 18:04:15 -0300

    Initial commit

---
+++ src/main/java/br/com/vitormelonunes/toll_customers/TollCustomersApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitormelonunes.toll_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollCustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vitormelonunes/toll_customers/TollCustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitormelonunes.toll_customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollCustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
